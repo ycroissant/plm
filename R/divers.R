302b4fcaab29f752662b7b8270d52fc17df7ca40
ycroissant <ycroissant@localhost>
2014-01-03 09:55:09 +0000

    has.intercept file is removed and content is in divers

--- R/divers.R
+++ R/divers.R
@@ -162,3 +162,24 @@ lev2var <- function(x, ...){
     z
   }
 }
+
+
+has.intercept <- function(object, ...) {
+  UseMethod("has.intercept")
+}
+
+has.intercept.default <- function(object, ...) {
+  has.intercept(formula(object), ...)
+}
+
+has.intercept.formula <- function(object, ...) {
+  attr(terms(object), "intercept") == 1L
+}
+
+has.intercept.Formula <- function(object, rhs = NULL, ...) {
+  ## NOTE: return a logical vector of the necessary length
+  ## (which might be > 1)
+  if(is.null(rhs)) rhs <- 1:length(attr(object, "rhs"))
+  sapply(rhs, function(x) has.intercept(formula(object, lhs = 0, rhs = x)))
+}
+
