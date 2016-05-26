cde9b241caf8b137bba67bb5fee179b33701e551
kev <kev@localhost>
2016-05-26 09:08:06 +0000

    pres: fixed handling of pggls' FD models

--- R/divers.R
+++ R/divers.R
@@ -197,21 +197,25 @@ has.intercept.plm <- function(object, part = "first", ...){
 pres <- function(x) {  # pres.panelmodel
   ## extracts model residuals as pseries
 
-    ## extract indices
-    groupind<-attr(x$model, "index")[,1]  #attr(model.frame(x), "index")[[1]]
-    timeind<-attr(x$model, "index")[,2]   #attr(model.frame(x), "index")[[2]]
-    if(!is.null(x$args$model)) {  # fix to allow operation with pggls, pmg
-    ## Achim's fix
-     if(x$args$model == "fd") {
-       groupi <- as.numeric(groupind)
-       ## make vector =1 on first obs in each group, 0 elsewhere
-       selector <-groupi-c(0,groupi[-length(groupi)])
-       selector[1] <- 1 # the first must always be 1
-       ## eliminate first obs in time for each group
-       groupind <- groupind[!selector]
-       timeind <- timeind[!selector]
-     }
- }
+  ## extract indices
+  groupind <-attr(x$model, "index")[,1]   #attr(model.frame(x), "index")[[1]]
+  timeind  <-attr(x$model, "index")[,2]   #attr(model.frame(x), "index")[[2]]
+  
+  # fix to allow operation with pggls, pmg (NB: pmg? meant: plm?)
+  # [NB: one day, make this cleaner; with the describe framework?]
+  if (!is.null(x$args$model))                 maybe_fd <- x$args$model
+  if (!is.null(attr(x, "pmodel")$model.name)) maybe_fd <- attr(x, "pmodel")$model.name # this line is currently needed to detect pggls models
+  
+  ## Achim's fix: reduce id and time index to accomodate first-differences model's number of observations
+  if(exists("maybe_fd") && maybe_fd == "fd") {
+    groupi <- as.numeric(groupind)
+    ## make vector =1 on first obs in each group, 0 elsewhere
+    selector <-groupi-c(0,groupi[-length(groupi)])
+    selector[1] <- 1 # the first must always be 1
+    ## eliminate first obs in time for each group
+    groupind <- groupind[!selector]
+    timeind <- timeind[!selector]
+  }
 
   resdata <- data.frame(ee=x$residuals, ind=groupind, tind=timeind)
   pee<-pdata.frame(resdata, index=c("ind","tind"))
