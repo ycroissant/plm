9f64a874e608e8ceb8c3f7c845ec7a965befc6e1
kev <kev@localhost>
2016-08-04 17:40:37 +0000

    test file: parameter should be 'model' not 'method'

--- tests/testEvaluation.R
+++ tests/testEvaluation.R
@@ -5,7 +5,8 @@ estimation_method <- "within"
 estimation_effect <- "individual"
 zz <- plm(log(gsp)~log(pcap)+log(pc)+log(emp)+unemp,
           data=Produc, 
-          index=c("state","year") , method=estimation_method , 
+          index=c("state","year"),
+          model=estimation_method, 
           effect=estimation_effect
           )
 print(summary(zz))
