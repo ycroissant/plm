33f2b24dc652b06a637b7dd939287ef1ef41a999
kev <kev@localhost>
2018-02-26 20:23:12 +0000

    update pgmm test with correct Arellano/Bond (1991) example and add .Rout.save

--- tests/test_pgmm.R
+++ tests/test_pgmm.R
@@ -1,27 +1,40 @@
 library("plm")
 data("EmplUK", package = "plm")
-## Arellano and Bond (1991), table 4b 
+## Arellano and Bond (1991), table 4 col. b
 z1 <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
-           + log(capital) + lag(log(output), 0:1) | lag(log(emp), 2:99)+lag(log(capital), 3:99),
+           + log(capital) + lag(log(output), 0:1) | lag(log(emp), 2:99),
             data = EmplUK, effect = "twoways", model = "twosteps")
 summary(z1)
-z1b <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
-           + log(capital) + lag(log(output), 0:1) | lag(log(emp), 2:99)+lag(log(capital), 3:99),
+
+z1col <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
+           + log(capital) + lag(log(output), 0:1) | lag(log(emp), 2:99),
+           data = EmplUK, effect = "twoways", model = "twosteps", collapse = TRUE)
+summary(z1col)
+
+
+z1ind <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
+           + log(capital) + lag(log(output), 0:1) | lag(log(emp), 2:99),
             data = EmplUK, effect = "individual", model = "twosteps")
-summary(z1b)
+summary(z1ind)
+
+z1indcol <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
+            + log(capital) + lag(log(output), 0:1) | lag(log(emp), 2:99),
+            data = EmplUK, effect = "individual", model = "twosteps")
+summary(z1indcol)
 
 
 ## Blundell and Bond (1998) table 4 (cf DPD for OX p.12 col.4)
+## not quite...
 z2 <- pgmm(log(emp) ~ lag(log(emp), 1)+ lag(log(wage), 0:1) +
            lag(log(capital), 0:1) | lag(log(emp), 2:99) +
-           lag(log(wage), 3:99) + lag(log(capital), 2:99),        
+           lag(log(wage), 3:99) + lag(log(capital), 2:99),
            data = EmplUK, effect = "twoways", model = "onestep", 
            transformation = "ld")
 summary(z2, robust = TRUE)
 
 z2b <- pgmm(log(emp) ~ lag(log(emp), 1)+ lag(log(wage), 0:1) +
            lag(log(capital), 0:1) | lag(log(emp), 2:99) +
-           lag(log(wage), 3:99) + lag(log(capital), 2:99),        
+           lag(log(wage), 3:99) + lag(log(capital), 2:99),
            data = EmplUK, effect = "individual", model = "onestep", 
            transformation = "ld")
 summary(z2b, robust = TRUE)
