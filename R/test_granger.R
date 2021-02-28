a7f671d7fbbb171219e8cec17f1a32b09f9bf5e1
kev <kev@localhost>
2021-02-28 09:23:27 +0000

    minors

--- R/test_granger.R
+++ R/test_granger.R
@@ -113,25 +113,25 @@ pgrangertest <- function(formula, data, test = c("Ztilde", "Zbar", "Wbar"), orde
   N <- pdim$nT$n
   T. <- pdim$nT$T
   Ti <- pdim$Tint$Ti
-  indi <- index(data)[[1]]
+  indi <- index(data)[[1L]]
   indi_con <- is.pconsecutive(data)
   
   # some input checks
-  if (!inherits(formula, "formula") || length(all.vars(formula)) > 2) {
+  if (!inherits(formula, "formula") || length(all.vars(formula)) > 2L) {
     stop(paste0("Argument 'formula' must be of class \"formula\" and may not contain ",
                 "more than 2 variables, one LHS and one RHS variable, e.g., 'y ~ x'"))
   }
   
-  if (!(is.numeric(order) && all(round(order) == order) && all(order > 0))) 
+  if (!(is.numeric(order) && all(round(order) == order) && all(order > 0L))) 
     stop("Lag order 'order' must contain positive integer(s)")
   
-  if (length(order) > 1 && length(order) != N) stop("'order' must have length 1 or the number of individuals")
+  if (length(order) > 1L && length(order) != N) stop("'order' must have length 1 or the number of individuals")
   if (test == "Zbar" && !balanced) stop("'test = \"Zbar\"' is not suited for unbalanced panels")
   if (test == "Zbar" && length(unique(order)) != 1) stop("'test = \"Zbar\"' is not suited for varying lag order")
   
   # For statistic Ztilde, the second order moments of the individual statistics must exist
   # (formula (10) in Dumitrescu/Hurlin (2012) where T = T - K)
-  if (length(order) == 1) {
+  if (length(order) == 1L) {
     if (test == "Ztilde" && !all((Ti > (5 + 3*order)))) {
       stop(paste0("Condition for test = \"Ztilde\" not met for all individuals: length of time series ",
                   "must be larger than 5+3*order (>5+3*", order, "=", 5 + 3*order,")"))
@@ -147,12 +147,12 @@ pgrangertest <- function(formula, data, test = c("Ztilde", "Zbar", "Wbar"), orde
   if (!all(indi_con)) {
     indnames <- pdim[["panel.names"]][["id.names"]]
     wrn1 <- "pgrangertest: result may be unreliable due to individuals with non-consecutive time periods: "
-    wrn2 <- if (sum(!indi_con) <= 5) {
+    wrn2 <- if (sum(!indi_con) <= 5L) {
       paste0(indnames[!indi_con], collapse = ", ") 
     }
     else { # cut off enumeration of individuals in warning message if more than 5
-      breakpoint <- which(cumsum(!indi_con) == 5)[1]
-      paste0(paste0(indnames[1:breakpoint][!indi_con[1:breakpoint]], collapse = ", "), ", ...")
+      breakpoint <- which(cumsum(!indi_con) == 5L)[1L]
+      paste0(paste0(indnames[1L:breakpoint][!indi_con[1L:breakpoint]], collapse = ", "), ", ...")
     }
     wrn <- paste0(wrn1, wrn2)
     warning(wrn)
@@ -165,7 +165,7 @@ pgrangertest <- function(formula, data, test = c("Ztilde", "Zbar", "Wbar"), orde
   
   # for this, if necessary, expand order argument for lmtest::grangertest to full length (N)
   # [but leave variable 'order' in its current length for later decision making]
-  if (length(order) == 1) order_grangertest <- rep(order, N) else order_grangertest <- order
+  if (length(order) == 1L) order_grangertest <- rep(order, N) else order_grangertest <- order
   
   # Dumitrescu/Hurlin (2012), p. 1453 use the Chisq definition of the Granger test
   grangertests_i <- mapply(function(data, order)
@@ -174,16 +174,16 @@ pgrangertest <- function(formula, data, test = c("Ztilde", "Zbar", "Wbar"), orde
     listdata, order_grangertest, SIMPLIFY = FALSE)
   
   # extract Wald/Chisq-statistics and p-values of individual Granger tests
-  Wi   <- lapply(grangertests_i, function(g) g[["Chisq"]][2])
-  pWi  <- lapply(grangertests_i, function(g) g[["Pr(>Chisq)"]][[2]])
-  dfWi <- lapply(grangertests_i, function(g) abs(g[["Df"]][2]))
+  Wi   <- lapply(grangertests_i, function(g) g[["Chisq"]][2L])
+  pWi  <- lapply(grangertests_i, function(g) g[["Pr(>Chisq)"]][[2L]])
+  dfWi <- lapply(grangertests_i, function(g) abs(g[["Df"]][2L]))
   
   Wbar <- c("Wbar" = mean(unlist(Wi)))
   
   if(test == "Zbar") {
     stat <- c(sqrt(N/(2*order)) * (Wbar - order))
     names(stat) <- "Zbar"
-    pval <- 2*pnorm(abs(stat), lower.tail = F)
+    pval <- 2*pnorm(abs(stat), lower.tail = FALSE)
   }
   
   if(test == "Ztilde") {
@@ -193,15 +193,15 @@ pgrangertest <- function(formula, data, test = c("Ztilde", "Zbar", "Wbar"), orde
                   * ( (T. - 3*order - 3) / (T. - 3*order - 1) * Wbar - order))
     } else {
       # unbalanced and/or varying lag order
-      # unbal stat seems to reduce to the balanced case for balanced data but rather treat it seperately here
+      # unbal stat seems to reduce to the balanced case for balanced data but rather treat it separately here
       # formula (33) in Dumitrescu/Hurlin (2012), p. 1459
-      if (length(order) == 1) order <- rep(order, N) # replicate lag order for all invididuals
+      if (length(order) == 1) order <- rep(order, N) # replicate lag order for all individuals
       stat <- c(   sqrt(N) * ( Wbar - 1/N * sum( order * (Ti - 3*order - 1) / (Ti - 3*order - 3) )) 
                    * 1/sqrt( 1/N * sum( 2* order * ((Ti - 3*order - 1)^2 * (Ti - 2*order - 3)) / 
                                           ((Ti - 3*order - 3)^2 * (Ti - 3*order - 5)) ) ) )
     }
     names(stat) <- "Ztilde"
-    pval <- 2*pnorm(abs(stat), lower.tail = F)
+    pval <- 2*pnorm(abs(stat), lower.tail = FALSE)
   }
   
   if(test == "Wbar") {
@@ -213,8 +213,8 @@ pgrangertest <- function(formula, data, test = c("Ztilde", "Zbar", "Wbar"), orde
   # make data frame with individual Granger test results and lag order
   indgranger <- data.frame(indi[!duplicated(indi)], unlist(Wi),
                            unlist(pWi), unlist(dfWi), 
-                           (if(length(order) == 1) rep(order, N) else order))
-  colnames(indgranger) <- c(names(index(data))[1], "Chisq", "p-value", "df", "lag")
+                           (if(length(order) == 1L) rep(order, N) else order))
+  colnames(indgranger) <- c(names(index(data))[1L], "Chisq", "p-value", "df", "lag")
   
   RVAL <- list(statistic = stat,
                parameter = NULL,
