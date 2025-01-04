3c713785e2b26aa3d6ba633639077c64d844b3f3
tappek <77916431+tappek@users.noreply.github.com>
2025-01-05 00:42:22 +0100

    more references values

--- inst/tests/test_mtest.R
+++ inst/tests/test_mtest.R
@@ -9,10 +9,10 @@ ab.a1 <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
               + lag(log(capital), 0:2) + lag(log(output), 0:2) | lag(log(emp), 2:99),
               data = EmplUK, effect = "twoways", model = "onestep")
 
-mtest(ab.a1, 1, vcov = NULL) # -3.9394 xtabond manual
-mtest(ab.a1, 2, vcov = NULL) # -0.54239  xtabond manual
-mtest(ab.a1, 1, vcov = vcovHC) # -3.600 (reference DPD/Ox abest3.out)         / -3.5996 xtabond manual
-mtest(ab.a1, 2, vcov = vcovHC) # -0.516 (reference A/B and DPD/Ox abest3.out) / -0.51603 xtabond manual
+mtest(ab.a1, 1, vcov = NULL) # -3.9394   xtabond manual, example 1
+mtest(ab.a1, 2, vcov = NULL) # -0.54239  xtabond manual, example 1
+mtest(ab.a1, 1, vcov = vcovHC) # -3.600 (reference DPD/Ox abest3.out)         / -3.5996 xtabond manual, example 2
+mtest(ab.a1, 2, vcov = vcovHC) # -0.516 (reference A/B and DPD/Ox abest3.out) / -0.51603 xtabond manual, example 2
 
 # Windmeijer (2025), table 2, onestep with corrected std. err
 # A/B (1991) model from col. b BUT as one-step model
@@ -21,8 +21,8 @@ ab.b.onestep <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
                 data = EmplUK, effect = "twoways", model = "onestep")
 # NB: coef match, non-robust std. errors do not match abest1.out
 
-mtest(ab.b.onestep, 1, vcov = NULL) # -3.409 (reference in DPD/Ox abest1.out)
-mtest(ab.b.onestep, 2, vcov = NULL) # 0.3695 (reference in DPD/Ox abest1.out)
+mtest(ab.b.onestep, 1, vcov = NULL) # -3.409  (reference in DPD/Ox abest1.out)
+mtest(ab.b.onestep, 2, vcov = NULL) # -0.3695 (reference in DPD/Ox abest1.out)
 mtest(ab.b.onestep, 1, vcov = vcovHC) # -2.493 (reference in Windmeijer and DPD/Ox abest1.out)
 mtest(ab.b.onestep, 2, vcov = vcovHC) # -0.359 (reference in Windmeijer and DPD/Ox abest1.out)
 
@@ -37,15 +37,15 @@ ab.a2 <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
 
 mtest(ab.a2, 1, vcov = NULL) # -3.000 (reference DPD/Ox in abest3.out)
 mtest(ab.a2, 2, vcov = NULL) # -0.434 (reference A/B) / DPD/Ox has -0.4158 in abest3.out, see comment in footnote 7, p. 32
-mtest(ab.a2, 1, vcov = vcovHC)
-mtest(ab.a2, 2, vcov = vcovHC)
+mtest(ab.a2, 1, vcov = vcovHC) # -2.1255  xtabond manual, example 4
+mtest(ab.a2, 2, vcov = vcovHC) # -0.35166 xtabond manual, example 4
 
 ## Arellano and Bond (1991), table 4 col. b / Windmeijer (2025), table 2
 ab.b <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
              + log(capital) + lag(log(output), 0:1) | lag(log(emp), 2:99),
              data = EmplUK, effect = "twoways", model = "twosteps")
 
-mtest(ab.b, 1, vcov = NULL)   # -2.826 (reference about A/B's m1 in Windmeijer) / DPD/Ox has -2.486
-mtest(ab.b, 2, vcov = NULL)   # -0.327 (reference in A/B and in Windmeijer)     / DPD/Ox has -0.3325, explanation in footnote 7 on p. 32
+mtest(ab.b, 1, vcov = NULL)   # -2.826 (reference about A/B's m1 in Windmeijer) / DPD/Ox has -2.428 in abest1.out
+mtest(ab.b, 2, vcov = NULL)   # -0.327 (reference in A/B and in Windmeijer)     / DPD/Ox has -0.3325 in abest1.out, explanation in footnote 7 on p. 32
 mtest(ab.b, 1, vcov = vcovHC) # -1.999 (reference in Windmeijer)
 mtest(ab.b, 2, vcov = vcovHC) # -0.316 (reference in Windmeijer)
