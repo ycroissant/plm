0ff8b1fa5949c27b54146152ae0faf7f4f31f3d1
tappek <77916431+tappek@users.noreply.github.com>
2025-01-04 16:19:24 +0100

    add more test cases

--- inst/tests/test_pgmm.R
+++ inst/tests/test_pgmm.R
@@ -1,13 +1,13 @@
 library("plm")
 data("EmplUK", package = "plm")
 
-# Arellano/Bond 1991, Table 4, column (a1) [non-robust SEs]
+# Arellano/Bond 1991, Table 4, column (a1) (has robust SEs)
 ab.a1 <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
               + lag(log(capital), 0:2) + lag(log(output), 0:2) | lag(log(emp), 2:99),
               data = EmplUK, effect = "twoways", model = "onestep")
 
-(s.ab.a1  <- summary(ab.a1, robust = FALSE)) # as tabulated by Arellano/Bond
-(s.ab.a1r <- summary(ab.a1, robust = TRUE))
+(s.ab.a1  <- summary(ab.a1, robust = FALSE))
+(s.ab.a1r <- summary(ab.a1, robust = TRUE)) # as tabulated by Arellano/Bond
 
 ### Arellano-Bond test, Sargan test and Wald test yield different results for onestep
 ## xtabond2 result for robust mtest for col. a1
@@ -16,7 +16,7 @@ ab.a1 <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
 #Arellano-Bond test for AR(2) in first differences: z = -0.52 Pr > z = 0.606
 
 
-# Arellano/Bond 1991, Table 4, column (a2) [non-robust SEs]
+# Arellano/Bond 1991, Table 4, column (a2) (non-robust SEs)
 ab.a2 <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
               + lag(log(capital), 0:2) + lag(log(output), 0:2) | lag(log(emp), 2:99),
               data = EmplUK, effect = "twoways", model = "twosteps")
@@ -32,7 +32,7 @@ ab.b <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
 
 # Windmeijer (2025), table 2, onestep with corrected std. err
 # (Windmeijer's table header does not indicate that for one-step model these are
-# corrected std errors)
+# corrected std errors, but this can be varified when looking at the produced results)
 wind.s1 <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
              + log(capital) + lag(log(output), 0:1) | lag(log(emp), 2:99),
              data = EmplUK, effect = "twoways", model = "onestep")
@@ -50,7 +50,7 @@ ab.b.ind <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
 summary(ab.b.ind, robust = TRUE) # default
 
 
-## Blundell and Bond (1998) table 4 (cf DPD for OX p.12 col.4)
+## Blundell and Bond (1998) table 4 (cf. DPD for OX p.12 col.4)
 ## not quite...
 ## Maybe due to this: "The original implementation of system GMM
 ## (Blundell and Bond 1998) used H = I." (from Roodman 2009, p. 117)
@@ -103,41 +103,53 @@ Cigar$real_c     <- Cigar$sales * Cigar$pop / Cigar$pop16
 Cigar$real_p     <- Cigar$price / Cigar$cpi * 100
 Cigar$real_pimin <- Cigar$pimin / Cigar$cpi * 100
 Cigar$real_ndi   <- Cigar$ndi   / Cigar$cpi
+Cigar$log_real_c     <- log(Cigar$real_c)
+Cigar$log_real_p     <- log(Cigar$real_p)
+Cigar$log_real_pimin <- log(Cigar$real_pimin)
+Cigar$log_real_ndi   <- log(Cigar$real_ndi)
 
+year.d <- contr.treatment(levels(factor(Cigar$year)))
+year.d <- cbind("63" = c(1, rep(0, nrow(year.d)-1)), year.d) # add base level
+colnames(year.d) <- paste0("year", colnames(year.d)) # make colnames
+year.d <- cbind("year" = rownames(year.d), as.data.frame(year.d)) # add column with year as numeric (to enable merge in next step) and make final data frame
+
+Cigar <- merge(Cigar, year.d, by = "year", sort = FALSE, all.x = TRUE, all.y = FALSE)
+Cigar <- Cigar[ , c(2,1, 3:length(names(Cigar)))] # order columns
+pCigar <- pdata.frame(Cigar, index = c("state", "year"))
 
 # Baltagi (2005, 3rd edition), table 8.1 [one-step not contained in later editions anymore]
 # GMM-one-step
-form_cig_GMMonestep <- log(real_c) ~ lag(log(real_c)) + log(real_p) + log(real_pimin) + log(real_ndi) | lag(log(real_c), 2:99)
-gmm_onestep <- pgmm(form_cig_GMMonestep, data = Cigar, effect = "twoways", model = "onestep")
-(sgmm_onestep <- summary(gmm_onestep, robust = FALSE))
-round(sgmm_onestep$coefficients[ , 1], 3)
-round(sgmm_onestep$coefficients[ , 3], 3) 
+form_cig_GMMonestep_tw <- formula(log(real_c) ~ lag(log(real_c)) + log(real_p) + log(real_pimin) + log(real_ndi) 
+                               | lag(log(real_c), 2:99)
+                               | log(real_p) + log(real_pimin) + log(real_ndi) )
 
-# matches coefficients in table 8.1: 0.84, -0.377, -0.016, 0.14
-# but not t-statistics
-#  t-statistics in parenthesis:      (52.0) (11.7) (0.4) (3.8)
-#### TODO: maybe matrix is a different one?
-# Keane/Neal (2016). p. 529 replicate by 
+gmm_onestep_tw <- pgmm(form_cig_GMMonestep_tw, data = Cigar, effect = "twoways", model = "onestep")
+(sgmm_onestep_tw <- summary(gmm_onestep_tw, robust = TRUE))
+round(sgmm_onestep_tw$coefficients[ , 1], 3)
+round(sgmm_onestep_tw$coefficients[ , 3], 3) 
+
+# matches coefficients in table 8.1:    0.84, -0.377, -0.016, 0.14
+# but not t-statistics in parenthesis:  (52.0) (11.7) ( 0.4)  (3.8)
+#
+# Keane/Neal (2016). p. 529 replicate by (small diff in statistics vs. Baltagi)
 # xtabond2 logc l.logc logp logpn logy i.yr, gmm(l.logc) iv(logp logpn logy i.yr) nolevel
 # What they call DIFF-GMM is “GMM-one-step” in Baltagi (2005)
 # and give 3 digits of coefficients:
 #                                   0.843  −0.377  −0.016 0.139
 #                                   (52.66)  (−11.81)  (−0.39) (3.88)
 
-# individual onestep (without time dummies)
+# individual onestep without time dummies:
 # Keane/Neal (2016). p. 529 also give individual model (without time dummies) by
 # xtabond2 logc l.logc logp logpn logy, gmm(l.logc) iv(logp logpn logy) nolevel
-gmm_onestep_ind <- pgmm(form_cig_GMMonestep, data = Cigar, effect = "individual", model = "onestep")
-
-(sgmm_onestep_ind <- summary(gmm_onestep_ind, robust = FALSE))
-
-round(sgmm_onestep_ind$coefficients[ , 1], 3)
-round(sgmm_onestep_ind$coefficients[ , 3], 3)
-## -> matches coefficients
-# TODO: statistic
 # 0.891  −0.152  0.050  −0.071
 # (47.04) (−5.49) (1.85) (−6.09)
-
+form_cig_GMMonestep_ind <- formula(log(real_c) ~ lag(log(real_c)) + log(real_p) + log(real_pimin) + log(real_ndi)
+                                   | lag(log(real_c), 2:99))
+gmm_onestep_ind <- pgmm(form_cig_GMMonestep_ind, data = pCigar, effect = "individual", model = "onestep")
+(sgmm_onestep_ind <- summary(gmm_onestep_ind, robust = TRUE))
+round(sgmm_onestep_ind$coefficients[ , 1], 3)
+round(sgmm_onestep_ind$coefficients[ , 3], 3)
+## -> matches coefficients, but not statistics
 
 # Replicate OLS and 2-way FE from Baltagi table 8.1
 form_cig_OLS <- log(real_c) ~ lag(log(real_c)) + log(real_p) + log(real_pimin) + log(real_ndi)
@@ -153,14 +165,6 @@ summary(plm(form_cig_OLS, data= Cigar, model="within", effect = "twoways"))
 #           iv(lnrp lnrpn lndrdi dum3 dum8 dum10-29) noleveleq robust nomata twostep
 # No of obs 1288, no of groups = 46, balanced, 28 obs/group, no of instruments = 53
 
-year.d <- contr.treatment(levels(factor(Cigar$year)))
-year.d <- cbind("63" = c(1, rep(0, nrow(year.d)-1)), year.d) # add base level
-colnames(year.d) <- paste0("year", colnames(year.d)) # make colnames
-year.d <- cbind("year" = rownames(year.d), as.data.frame(year.d)) # add column with year as numeric (to enable merge in next step) and make final data frame
-
-pCigar <- merge(Cigar, year.d, by = "year", sort = FALSE, all.x = TRUE, all.y = FALSE)
-pCigar <- pCigar[ , c(2,1, 3:length(names(pCigar)))] # order columns
-pCigar <- pdata.frame(pCigar, index = c("state", "year"))
 
 # not quite (need to add IV instruments!?):
 form2021 <- formula(log(real_c) ~ lag(log(real_c)) + log(real_p) + log(real_pimin) + log(real_ndi) +
@@ -174,7 +178,7 @@ form2021 <- formula(log(real_c) ~ lag(log(real_c)) + log(real_p) + log(real_pimi
   year84 + year85 + year86 + year87 + year88 + year89 + 
   year90 + year91
 #  + year92
- | lag(log(real_c), 2:99)
+ | lag(log(real_c), 2:99) | log(real_p) + log(real_pimin) + log(real_ndi)
 )
 
 gmm_twostep <- pgmm(form2021, data = pCigar, effect = "individual", model = "twosteps", transformation = "d", collapse = TRUE)
@@ -258,6 +262,29 @@ if(pdynmc.avail) {
     estimation = "twostep", opt.meth = "none")
   summary(ab.a2r.pdynmc)
   
+  
+  ab.b.pdynmc <- pdynmc(
+    dat = dat, varname.i = "firm", varname.t = "year",
+    use.mc.diff = TRUE, use.mc.lev = FALSE, use.mc.nonlin = FALSE,
+    include.y = TRUE, varname.y = "n", lagTerms.y = 2,
+    fur.con = TRUE, fur.con.diff = TRUE, fur.con.lev = FALSE,
+    varname.reg.fur = c("w", "k", "ys"), lagTerms.reg.fur = c(1,0,1),
+    include.dum = TRUE, dum.diff = TRUE, dum.lev = FALSE, varname.dum = "year",
+    w.mat = "iid.err", std.err = "unadjusted",
+    estimation = "twostep", opt.meth = "none")
+  summary(ab.b.pdynmc)
+  
+  ab.br.pdynmc <- pdynmc(
+    dat = dat, varname.i = "firm", varname.t = "year",
+    use.mc.diff = TRUE, use.mc.lev = FALSE, use.mc.nonlin = FALSE,
+    include.y = TRUE, varname.y = "n", lagTerms.y = 2,
+    fur.con = TRUE, fur.con.diff = TRUE, fur.con.lev = FALSE,
+    varname.reg.fur = c("w", "k", "ys"), lagTerms.reg.fur = c(1,0,1),
+    include.dum = TRUE, dum.diff = TRUE, dum.lev = FALSE, varname.dum = "year",
+    w.mat = "iid.err", std.err = "corrected",
+    estimation = "twostep", opt.meth = "none")
+  summary(ab.br.pdynmc)
+  
   ### check coefs and SE pgmm vs. pdynmc ###
   
   ## one-step GMM
@@ -276,21 +303,41 @@ if(pdynmc.avail) {
   stopifnot(isTRUE(all.equal(round( s.ab.a2[[1L]][ , 2], 5), round( ab.a2.pdynmc$stderr$step2[1:10], 5), check.attributes = FALSE)))
   stopifnot(isTRUE(all.equal(round(s.ab.a2r[[1L]][ , 2], 5), round(ab.a2r.pdynmc$stderr$step2[1:10], 5), check.attributes = FALSE)))
   
+  # check coefficients
+  stopifnot(isTRUE(all.equal(round( s.ab.b[[1L]][ , 1], 5), round( ab.b.pdynmc$coefficients[1:7], 5), check.attributes = FALSE)))
+  
+  # check standard errors (non-robust and robust)
+  stopifnot(isTRUE(all.equal(round( s.ab.b[[1L]][ , 2], 5), round( ab.b.pdynmc$stderr$step2[1:7], 5), check.attributes = FALSE)))
+  stopifnot(isTRUE(all.equal(round(s.ab.br[[1L]][ , 2], 5), round(ab.br.pdynmc$stderr$step2[1:7], 5), check.attributes = FALSE)))
+  
+  
   ## Baltagi table 8.1
-  data("Cigar", package = "plm")
-  Cigar$real_c     <- log((Cigar$sales*Cigar$pop) / Cigar$pop16)
-  Cigar$real_p     <- log(Cigar$price/Cigar$cpi * 100)
-  Cigar$real_pimin <- log(Cigar$pimin/Cigar$cpi * 100)
-  Cigar$real_ndi   <- log(Cigar$ndi/Cigar$cpi)
   
   ## TODO: does not replicate literature yet
   c1 <- pdynmc(dat = Cigar, varname.i = "state", varname.t = "year",
                use.mc.diff = TRUE, use.mc.lev = FALSE, use.mc.nonlin = FALSE,
                include.y = TRUE, varname.y = "real_c", lagTerms.y = 1,
                fur.con = TRUE, fur.con.diff = TRUE, fur.con.lev = FALSE,
-               varname.reg.fur = c("real_p", "real_pimin", "real_ndi"), lagTerms.reg.fur = c(0,0,0),
-               include.dum = TRUE, dum.diff = TRUE, dum.lev = FALSE, varname.dum = "year",
-               w.mat = "iid.err", std.err = "unadjusted", estimation = "onestep",
+               varname.reg.fur = c("real_p", "real_pimin", "real_ndi",
+                                   # "year65", "year70", paste0("year", 72:91)), # Baltagi 2013, 2021
+                                   paste0("year", 65:91)),  # Baltagi 2005
+               lagTerms.reg.fur = c(rep(0, 3), rep(0, 27)), # Baltagi 2005
+               include.dum = FALSE,
+               inst.collapse = FALSE,
+               w.mat = "iid.err", std.err = "corrected", estimation = "twostep",
                opt.meth = "none")
   summary(c1)
+  
+  # onestep without time dummies
+  onestep_ind <- pdynmc(dat = Cigar, varname.i = "state", varname.t = "year",
+                        use.mc.diff = TRUE, use.mc.lev = FALSE, use.mc.nonlin = FALSE,
+                        include.y = TRUE, varname.y = "log_real_c", lagTerms.y = 1,
+                        fur.con = TRUE, fur.con.diff = TRUE, fur.con.lev = FALSE,
+                        varname.reg.fur = c("log_real_p", "log_real_pimin", "log_real_ndi"),
+                        lagTerms.reg.fur = c(rep(0, 3)),
+                        include.dum = FALSE,
+                        inst.collapse = FALSE,
+                        w.mat = "iid.err", std.err = "corrected", estimation = "onestep",
+                        opt.meth = "none")
+  summary(onestep_ind)
 }
